ad4c12f457ad08fa2f34c43bb70bdd20ed580b3f
Alan Foster <alan-af@outlook.com>
2023-01-27 05:36:19 -0300

    5

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const { validadeUser } = require('../middleware/userMiddleware');
+const { validateToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/user', validadeUser, userController.addUser);
+router.get('/user', validateToken, userController.getAllUser);
 
 module.exports = router;    
