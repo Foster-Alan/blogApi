d64ef28938b2ff5244b1a2a9637c146a7041b38b
Alan Foster <alan-af@outlook.com>
2023-01-27 08:42:10 -0300

    14

--- src/router/postRouter.js
+++ src/router/postRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const { validateToken } = require('../middleware/authMiddleware');
 const { validatePost } = require('../middleware/postMiddleware');
-const { getAllPost, addPost } = require('../controller/postController');
+const { getAllPost, addPost, getPostById } = require('../controller/postController');
 
 const router = express.Router();
 
 router.get('/post', validateToken, getAllPost);
-
 router.post('/post', validateToken, validatePost, addPost);
+router.get('/post/:id', validateToken, getPostById);
 
 module.exports = router;
