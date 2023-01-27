2ae25418510b5aa00c952ffd0dbedbbee9d764de
Alan Foster <alan-af@outlook.com>
2023-01-27 06:36:31 -0300

    9

--- src/router/categoryRouter.js
+++ src/router/categoryRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const categoryController = require('../controller/categoryController');
+const { addCategory, getAllCategories } = require('../controller/categoryController');
 
 const { validateToken } = require('../middleware/authMiddleware');
-const categoryMiddleware = require('../middleware/categoryMiddleware');
+const { validadeCategory } = require('../middleware/categoryMiddleware');
 
 const router = express.Router();
 
-router.post('/categories', validateToken, categoryMiddleware.validadeCategory, 
-categoryController.addCategory);
+router.post('/categories', validateToken, validadeCategory, addCategory);
+router.get('/categories', validateToken, getAllCategories);
 
 module.exports = router;
